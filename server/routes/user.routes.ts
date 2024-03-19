b7e13478aee40bff71b05719fecd016ad53e1b27
Rizqi <rizqiilhamsiregar@gmail.com>
2024-03-19 12:32:02 +0700

    env

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { activateUser, registrationUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
-userRouter.post("/register", registrationUser);
+userRouter.post("/registration", registrationUser);
 
 userRouter.post("/activate-user", activateUser);
 
