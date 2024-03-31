a93302e5385bb730bfb96e971e7f076e37fe926f
Rizqi <rizqiilhamsiregar@gmail.com>
2024-03-31 18:57:12 +0700

    User authentication & authorization

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -8,6 +8,7 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 // register user
 interface IRegistrationBody {
@@ -156,6 +157,11 @@ export const logoutUser = catchAsyncError(async(req: Request, res:Response, next
   try{
     res.cookie("access_token","", {maxAge:1});
     res.cookie("refresh_token","", {maxAge:1});
+    const userId = req.user?.id || ''
+
+    redis.del(userId);
+
+
     res.status(200).json({
       success:true,
       message: "Logged out successfully",
