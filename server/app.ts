67b4de96a1b5bf8a79832155067e28f8f74fdefa
Rizqi <rizqiilhamsiregar@gmail.com>
2024-04-03 12:39:01 +0700

    add Course Model, Create course, Edit course

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.routes";
+import courseRouter from "./routes/course.routes";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -20,7 +21,9 @@ app.use(
 );
 
 //routes
-app.use("/api/v1",userRouter);
+app.use("/api/v1", userRouter);
+
+app.use("/api/v1", courseRouter);
 
 //testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
@@ -38,4 +41,4 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
 });
 
 
-app.use (ErrorMiddleware);
+app.use(ErrorMiddleware);
